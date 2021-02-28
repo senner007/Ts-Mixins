170e970fccbce8e5a0e173d3a55b6c9c56ce64ea
nig <nig@beepanalytics.com>
2021-02-28 12:08:27 +0100

    wip

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,19 @@ function Operational<BaseClass extends Constructable<{}>>(Base: BaseClass) {
 
 function ReadClass<BaseClass extends Constructable<ReadJob>>(Base: BaseClass) {
   return class extends Base {
-    readFunc: IReadOperation<EReadCommand> = async (command: EReadCommand) => EReadCommand[command];
+    readFunc: IReadOperation<EReadCommand> = async (command: EReadCommand) =>
+      EReadCommand[command];
   };
 }
 
-function WriteClass<BaseClass extends Constructable<WriteJob>>(Base: BaseClass) {
-    return class extends Base {
-      writeFunc: IReadOperation<EWriteCommand> = async (command: EWriteCommand) => EWriteCommand[command];
-    };
-  }
-
+function WriteClass<BaseClass extends Constructable<WriteJob>>(
+  Base: BaseClass
+) {
+  return class extends Base {
+    writeFunc: IReadOperation<EWriteCommand> = async (command: EWriteCommand) =>
+      EWriteCommand[command];
+  };
+}
 
 class WriteJob implements IWriteJob {
   param: EWriteCommand;
@@ -51,63 +54,74 @@ enum EWriteCommand {
 }
 
 class OtherOperation {
-  action : () => void;
-  constructor(obj: {action : () => void }) {
+  action: () => void;
+  constructor(obj: { action: () => void }) {
+    this.action = obj.action;
+  }
+}
+
+class OtherIdenticalOperation {
+  action: () => void;
+  constructor(obj: { action: () => void }) {
     this.action = obj.action;
   }
 }
 
 class OtherOperationAsync {
-  func : () => Promise<string>;
-  constructor(obj: {func : () => Promise<string> }) {
+  func: () => Promise<string>;
+  constructor(obj: { func: () => Promise<string> }) {
     this.func = obj.func;
   }
 }
 
 function voidFunction() {
-  console.log("not returning anything")
+  console.log('not returning anything');
 }
 
 async function stringPromiseFunction() {
   return new Promise((resolve: (value: string) => void) => {
-    resolve("promising a string")
-  })
+    resolve('promising a string');
+  });
 }
 
 const OperationalRead = Operational(ReadClass(ReadJob));
 const OperationalWrite = Operational(WriteClass(WriteJob));
 
-async function executor(
-  jobs: (
-    | InstanceType<typeof OperationalRead>
-    | InstanceType<typeof OperationalWrite>
-    | OtherOperation
-    | OtherOperationAsync
-  )[]
-) {
+type TypeExecutorJob = (
+  | InstanceType<typeof OperationalRead>
+  | InstanceType<typeof OperationalWrite>
+  | OtherOperation
+  | OtherOperationAsync
+)[];
+
+async function executor(jobs: TypeExecutorJob) {
   for (const job of jobs) {
     if (job instanceof ReadJob) {
       const result = await job.readFunc(job.param);
-      console.log(job)
+      console.log(job);
       for (const postCommand of job.postCommands) {
-          console.log(postCommand())
+        console.log(postCommand());
       }
       console.log(result);
     }
     if (job instanceof WriteJob) {
-        console.log(job)
-        for (const postCommand of job.postCommands) {
-            console.log(postCommand())
-        }
-        const result = await job.writeFunc(job.param);
-        console.log(result);
+      console.log(job);
+      for (const postCommand of job.postCommands) {
+        console.log(postCommand());
+      }
+      const result = await job.writeFunc(job.param);
+      console.log(result);
     }
     if (job instanceof OtherOperation) {
-      console.log(job)
+      console.log(job);
+      job.action();
+    }
+    if (job instanceof OtherIdenticalOperation) {
+      console.log(job);
       job.action();
     }
     if (job instanceof OtherOperationAsync) {
-      console.log(job)
+      console.log(job);
       const result = await job.func();
       console.log(result);
     }
@@ -122,9 +136,36 @@ executor([
     param: EWriteCommand.MCU,
   }),
   new OtherOperation({
-    action : voidFunction,
+    action: voidFunction,
+  }),
+  new OtherIdenticalOperation({
+    action: voidFunction,
   }),
   new OtherOperationAsync({
-    func : stringPromiseFunction,
-  })
+    func: stringPromiseFunction,
+  }),
 ]);
+
+
+// ReadJob {
+//   param: 0,
+//   readFunc: [AsyncFunction (anonymous)],
+//   postCommands: [ [Function (anonymous)], [Function (anonymous)] ]
+// }
+// foo
+// bar
+// DATA
+// WriteJob {
+//   param: 0,
+//   writeFunc: [AsyncFunction (anonymous)],
+//   postCommands: [ [Function (anonymous)], [Function (anonymous)] ]
+// }
+// foo
+// bar
+// MCU
+// OtherOperation { action: [Function: voidFunction] }
+// not returning anything
+// OtherIdenticalOperation { action: [Function: voidFunction] }
+// not returning anything
+// OtherOperationAsync { func: [AsyncFunction: stringPromiseFunction] }
+// promising a string
